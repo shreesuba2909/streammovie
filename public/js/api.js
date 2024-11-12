8889fb3bddc24cc934673522a5bba74e265690f6
shreesuba2909 <suba.jxk09@gmail.com>
2024-11-12 16:32:35 +0530

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+
+let api_key = "05dfd6f37e1403412139893386ff723d";
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
+
